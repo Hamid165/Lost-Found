e146311a5ca77569b710a7914a425903a36bc828
Yuchan162 <hamidskj123@gmail.com>
2025-11-11 11:55:34 +0700

    logo

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,60 +1,76 @@
 <footer class="bg-red-800 py-16 mt-10">
-    <div class="container mx-auto px-8">
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 text-center md:text-left gap-y-10 lg:gap-y-0 md:gap-x-4 lg:gap-x-6">
+  <div class="container mx-auto px-8">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 text-center md:text-left gap-y-10 md:gap-x-6">
 
-            {{-- Kontak --}}
-            <div class="p-4">
-                <h3 class="text-lg font-bold mb-4 text-white">Kontak</h3>
-                <div class="flex flex-col items-center md:items-start space-y-3">
-                    <a href="#" class="border border-red-700 rounded-lg px-4 py-2 text-sm w-full sm:w-auto text-white hover:bg-red-700 transition">
-                        <p>+62 811 444 8585</p>
-                    </a>
-                    <a href="#" class="border border-red-700 rounded-lg px-4 py-2 text-sm w-full sm:w-auto text-white hover:bg-red-700 transition">
-                        <p>lostfound@gmail.com</p>
-                    </a>
-                </div>
-            </div>
-
-            {{-- Jelajahi --}}
-            <div class="p-4">
-                <h3 class="text-lg font-bold mb-4 text-white">Jelajahi</h3>
-                <ul class="space-y-2 text-sm text-gray-200">
-                    <li><a href="#" class="hover:text-white hover:underline">Kebijakan Privasi</a></li>
-                    <li><a href="#" class="hover:text-white hover:underline">Syarat dan Ketentuan</a></li>
-                    <li><a href="#" class="hover:text-white hover:underline">Pusat Bantuan</a></li>
-                </ul>
-            </div>
-
-            {{-- Sosial Media --}}
-            <div class="p-4">
-                <h3 class="text-lg font-bold mb-4 text-white">Sosial Media</h3>
-                <div class="flex justify-center md:justify-start space-x-4 mb-4">
-                    <a href="#" class="text-white hover:text-gray-300">
-                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" /></svg>
-                    </a>
-                    <a href="#" class="text-white hover:text-gray-300">
-                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.048-1.067-.06-1.407-.06-4.123v-.08c0-2.643.012-2.987.06-4.043.049 1.064.218 1.791.465 2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 016.341 2.52c.636-.247 1.363-.416 2.427-.465C9.793 2.013 10.148 2 12.575 2h.08zM12 8.25a3.75 3.75 0 100 7.5 3.75 3.75 0 000-7.5zM12 13.5a1.5 1.5 0 110-3 1.5 1.5 0 010 3zm4.943-6.162a1.125 1.125 0 100-2.25 1.125 1.125 0 000 2.25z" clip-rule="evenodd" /></svg>
-                    </a>
-                    <a href="#" class="text-white hover:text-gray-300">
-                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                            <path d="M12.525 2.155a.7.7 0 01.378.077c.404.148.803.323 1.189.524 1.106.586 2.062 1.354 2.871 2.298.81.942 1.403 2.053 1.765 3.267a.71.71 0 01-.012.593l-.004.004-.001.001c-.163.153-.339.29-.526.417a.684.684 0 01-.703.018c-1.352-.61-2.906-.855-4.504-.855-2.022 0-3.668.423-4.508 1.258-.84.836-.889 2.29-1.284 4.547-.406 2.302-.734 4.577-1.127 6.643a.7.7 0 01-.762.531c-1.294-.367-2.732-.823-4.223-1.365a.71.71 0 01-.378-.077.7.7 0 01-.378-.44c-.201-.54-.393-1.089-.575-1.637a.71.71 0 01.012-.593l.004-.004c.163-.153.339-.29.526-.417a.684.684 0 01.703-.018c1.352.61 2.906.855 4.504.855 2.022 0 3.668-.423 4.508-1.258.84-.836.889-2.29 1.284-4.547.406 2.302.734 4.577 1.127-6.643a.7.7 0 01.762-.531c1.294.367 2.732.823 4.223 1.365a.71.71 0 01.378.077z" />
-                        </svg>
-                    </a>
-                </div>
-            </div>
-
-            {{-- Alamat --}}
-            <div class="p-4">
-                <h3 class="text-lg font-bold mb-4 text-white">Alamat</h3>
-                <div class="text-sm text-gray-200 space-y-3">
-                    <p> Jl. DI Panjaitan No.128, Karangreja, Purwokerto Kidul, Kec. Purwokerto Sel., Kabupaten Banyumas, Jawa Tengah 53147</p>
-                </div>
-            </div>
+      <!-- Kontak -->
+      <div class="p-4 flex flex-col items-center md:items-start">
+        <h3 class="text-lg font-bold mb-4 text-white">Kontak</h3>
+        <div class="flex flex-col items-center md:items-start space-y-3">
+          <a href="#" class="border border-red-700 rounded-lg px-4 py-2 text-sm text-white hover:bg-red-700 transition">
+            +62 811 444 8585
+          </a>
+          <a href="#" class="border border-red-700 rounded-lg px-4 py-2 text-sm text-white hover:bg-red-700 transition">
+            lostfound@gmail.com
+          </a>
         </div>
+      </div>
 
-        {{-- Copyright --}}
-        <div class="mt-16 border-t border-red-700 pt-8 text-center text-sm text-gray-300">
-            <p>&copy; {{ date('Y') }} Lost & Found Kampus. All Rights Reserved.</p>
+      <!-- Jelajahi -->
+      <div class="p-4">
+        <h3 class="text-lg font-bold mb-4 text-white">Jelajahi</h3>
+        <ul class="space-y-2 text-sm text-gray-200">
+          <li><a href="#" class="hover:text-white hover:underline">Kebijakan Privasi</a></li>
+          <li><a href="#" class="hover:text-white hover:underline">Syarat dan Ketentuan</a></li>
+          <li><a href="#" class="hover:text-white hover:underline">Pusat Bantuan</a></li>
+        </ul>
+      </div>
+
+      <!-- Sosial Media -->
+      <div class="p-4 flex flex-col items-center md:items-start">
+        <h3 class="text-lg font-bold mb-4 text-white">Sosial Media</h3>
+        <div class="flex justify-center md:justify-start gap-5">
+          <!-- Facebook -->
+          <a href="#" class="text-white hover:text-gray-300">
+            <div class="bg-white rounded-full p-2 flex items-center justify-center">
+              <svg class="w-5 h-5 text-red-800" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M22 12a10 10 0 10-11.5 9.9v-7h-2v-3h2v-2.3c0-2 1.2-3.1 3-3.1.9 0 1.8.16 1.8.16v2h-1c-1 0-1.3.63-1.3 1.27V12h2.3l-.37 3h-1.93v7A10 10 0 0022 12z" />
+              </svg>
+            </div>
+          </a>
+
+          <!-- Instagram -->
+          <a href="#" class="text-white hover:text-gray-300">
+            <div class="bg-white rounded-full p-2 flex items-center justify-center">
+              <svg class="w-5 h-5 text-red-800" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M7.75 2h8.5A5.75 5.75 0 0122 7.75v8.5A5.75 5.75 0 0116.25 22h-8.5A5.75 5.75 0 012 16.25v-8.5A5.75 5.75 0 017.75 2zm0 1.5A4.25 4.25 0 003.5 7.75v8.5A4.25 4.25 0 007.75 20.5h8.5A4.25 4.25 0 0020.5 16.25v-8.5A4.25 4.25 0 0016.25 3.5h-8.5zM12 7a5 5 0 110 10 5 5 0 010-10zm0 1.5a3.5 3.5 0 100 7 3.5 3.5 0 000-7zm4.88-.88a.88.88 0 110 1.76.88.88 0 010-1.76z" />
+              </svg>
+            </div>
+          </a>
+
+          <!-- TikTok -->
+          <a href="#" class="text-white hover:text-gray-300">
+            <div class="bg-white rounded-full p-2 flex items-center justify-center">
+              <svg class="w-5 h-5 text-red-800" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M12.75 3c.14 1.08.61 2.07 1.35 2.85A5.4 5.4 0 0016.8 7h.9v2.43a6.6 6.6 0 01-3.15-.78v5.7a4.65 4.65 0 11-4.65-4.65c.18 0 .36.01.54.03v2.43a2.25 2.25 0 102.25 2.25V3h2.06z" />
+              </svg>
+            </div>
+          </a>
         </div>
+      </div>
+
+      <!-- Alamat -->
+      <div class="p-4 flex flex-col items-center md:items-start">
+        <h3 class="text-lg font-bold mb-4 text-white">Alamat</h3>
+        <p class="text-sm text-gray-200 leading-relaxed">
+          Jl. DI Panjaitan No.128, Karangreja, Purwokerto Kidul, Kec. Purwokerto Sel., Kabupaten Banyumas,
+          Jawa Tengah 53147
+        </p>
+      </div>
     </div>
+
+    <!-- Copyright -->
+    <div class="mt-16 border-t border-red-700 pt-8 text-center text-sm text-gray-300">
+      <p>&copy; 2025 Lost & Found Kampus. All Rights Reserved.</p>
+    </div>
+  </div>
 </footer>
